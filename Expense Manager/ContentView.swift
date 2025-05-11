3de53aa3371f8d448cb2248ce524c6ef9ce5f5ee
Aditya Vyavahare <vvaditya23@gmail.com>
2025-05-11 10:30:11 +0530

    Initial Commit

---
+++ Expense Manager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Expense Manager
+//
+//  Created by Aditya on 11/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Expense Manager/Expense_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_ManagerApp.swift
+//  Expense Manager
+//
+//  Created by Aditya on 11/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Expense_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
