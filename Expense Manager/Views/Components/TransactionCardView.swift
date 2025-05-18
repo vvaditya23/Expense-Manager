e3679bc5ac55752c9050162f7ffef182d187f210
Aditya Vyavahare <vvaditya23@gmail.com>
2025-05-18 16:23:44 +0530

    Implement custom horizontal swipe action

--- Expense Manager/Views/Components/TransactionCardView.swift
+++ Expense Manager/Views/Components/TransactionCardView.swift
@@ -11,34 +11,46 @@ struct TransactionCardView: View {
     var transaction: Transaction
 
     var body: some View {
-        HStack(spacing: 12) {
-            Text(transaction.title.prefix(1))
-                .font(.title)
-                .fontWeight(.semibold)
-                .foregroundStyle(.white)
-                .frame(width: 45, height: 45)
-                .background(transaction.color.gradient, in: .capsule)
+        CustomSwipeAction(
+            cornerRadius: 10,
+            swipeDirection: .trailing
+        ) {
+            HStack(spacing: 12) {
+                Text(transaction.title.prefix(1))
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.white)
+                    .frame(width: 45, height: 45)
+                    .background(transaction.color.gradient, in: .capsule)
 
-            VStack(alignment: .leading, spacing: 4) {
-                Text(transaction.title)
-                    .foregroundStyle(Color.primary)
+                VStack(alignment: .leading, spacing: 4) {
+                    Text(transaction.title)
+                        .foregroundStyle(Color.primary)
 
-                Text(transaction.remarks)
-                    .font(.caption)
-                    .foregroundStyle(Color.primary.secondary)
+                    Text(transaction.remarks)
+                        .font(.caption)
+                        .foregroundStyle(Color.primary.secondary)
 
-                Text(format(date: transaction.dateAdded, format: "dd MMM yyyy"))
-                    .font(.caption2)
-                    .foregroundStyle(.gray)
+                    Text(format(date: transaction.dateAdded, format: "dd MMM yyyy"))
+                        .font(.caption2)
+                        .foregroundStyle(.gray)
+                }
+                .lineLimit(1)
+                .hSpacing(.leading)
+
+                Text(formatCurrency(transaction.amount))
             }
-            .lineLimit(1)
-            .hSpacing(.leading)
-
-            Text(formatCurrency(transaction.amount))
+            .padding(.horizontal, 15)
+            .padding(.vertical, 10)
+            .background(.background, in: .rect(cornerRadius: 10))
+        } actions: {
+            SwipeAction(
+                tint: .red,
+                icon: "trash"
+            ) {
+                // TODO: Delete
+            }
         }
-        .padding(.horizontal, 15)
-        .padding(.vertical, 10)
-        .background(.background, in: .rect(cornerRadius: 10))
     }
 }
 
